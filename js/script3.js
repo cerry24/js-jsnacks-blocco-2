91aa1bde8574e935f2dfb6baf2a2e2b287f02e18
cerry24 <samuelecerretti@gmail.com>
2022-11-25 17:13:36 +0100

    max array's first ten numbers

--- js/script3.js
+++ js/script3.js
@@ -1,5 +1,5 @@
 /**
- * Calcola la somma e la media dei primi 10 numeri di un array.
+ * Calcola la somma, la media e il valore massimo dei primi 10 numeri di un array.
  */
 
 
@@ -9,13 +9,19 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 let sum = 0;
 let average = 0;
+let max= 0;
 
 for ( i = 0; i < 10; i++) {
 
     sum += numbers[i];
+
+    if ( numbers[i] > max) {
+        max = numbers[i];
+    }
 }
 
 average = sum / 10
 
-console.log (sum)
-console.log (average)
+console.log (sum);
+console.log (average);
+console.log (max);
